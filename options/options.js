fee77ced59c5d9ed07bf3199adad9f4bec04ee7c
Crowdbotics <team@crowdbotics.com>
2021-11-25 20:10:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pdf_books_32144",
+  url: "https://pdf_books_32144.botics.co",
+  api: "https://pdf_books_32144.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
